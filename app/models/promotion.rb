548b31b095b1631da5befb1715ca221dfe638358
Khuong Mai <maingockhuong89@gmail.com>
2023-03-24 11:12:17 -0500

    validate promotion

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,2 +1,6 @@
 class Promotion < ApplicationRecord
+  validates :name, :discount_rate, presence: true
+
+  has_many :category_promotions, dependent: :delete_all
+  has_many :categories, through: :category_promotions
 end
