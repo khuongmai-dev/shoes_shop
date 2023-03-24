7ac7691d56775d05df39858cf225fe42599d41ef
Khuong Mai <maingockhuong89@gmail.com>
2023-03-24 15:33:51 -0500

    validate user_payment_method

--- app/models/user_payment_method.rb
+++ app/models/user_payment_method.rb
@@ -1,4 +1,8 @@
 class UserPaymentMethod < ApplicationRecord
+  validates :shop_user_id, :payment_type_id, :provider, :account_number, :expriry_date, presence: true
+
   belongs_to :shop_user
   belongs_to :payment_type
+
+  has_many :shop_orders, dependent: :destroy
 end
