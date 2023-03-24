1f22efe675c53edf71038f45cfb253af288e947e
Khuong Mai <maingockhuong89@gmail.com>
2023-03-24 12:10:53 -0500

    validate product_item

--- app/models/product_item.rb
+++ app/models/product_item.rb
@@ -1,3 +1,13 @@
 class ProductItem < ApplicationRecord
+
+  validate :price, :stock_quantity
   belongs_to :product
+
+  has_many :shopping_cart_items, dependent: :destroy
+
+  has_many :product_item_variation_options, dependent: :destroy
+  has_many :variation_options, through: :product_item_variation_options
+
+  has_many :order_lines, dependent: :destroy
+  has_many :shop_orders, through: :order_lines
 end
