d0f84c5f62541b4ae4d7689eaa8ce2d1bcdf9f2d
Khuong Mai <maingockhuong89@gmail.com>
2023-03-24 12:12:47 -0500

    validate variation_option model

--- app/models/variation_option.rb
+++ app/models/variation_option.rb
@@ -1,3 +1,7 @@
 class VariationOption < ApplicationRecord
+  validate :value, presence: true
   belongs_to :variation
+
+  has_many :product_item_variation_options, dependent: :destroy
+  has_many :product_item, through: :product_item_variation_options
 end
