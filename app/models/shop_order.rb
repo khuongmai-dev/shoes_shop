5f55bc5b0e5e10dbb10148bf37e234f504a24634
Khuong Mai <maingockhuong89@gmail.com>
2023-03-24 15:43:48 -0500

    validate shop_order

--- app/models/shop_order.rb
+++ app/models/shop_order.rb
@@ -1,7 +1,12 @@
 class ShopOrder < ApplicationRecord
+  validates :shop_user_id, :order_date, :user_payment_method_id, :address_id, :shipping_method_id, :tax_rate_id, :order_status_id
+
   belongs_to :user_payment_method
   belongs_to :address
   belongs_to :shipping_method
   belongs_to :tax_rate
   belongs_to :order_status
+
+  has_many :order_lines, dependent: :destroy
+  has_many :product_items, through: :order_lines
 end
