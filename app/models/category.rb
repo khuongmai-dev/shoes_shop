7af7ab428b5b71b3acef523a282e9d17bdd3a70a
Khuong Mai <maingockhuong89@gmail.com>
2023-03-24 11:49:31 -0500

    validate, self-reference Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,18 @@
 class Category < ApplicationRecord
+  validates :name, presence: true
+
+# Declare the self-referential relationship with parent and children categories
+  belongs_to :parent, class_name: 'Category', optional: true
+  has_many :children, class_name: 'Category', foreign_key: 'parent_id', dependent: :destroy
+
+  validates :name, uniqueness: { scope: :parent_id, case_sensitive: false }
+
+  # Validate that parent category cannot be self
+  validate :parent_cannot_be_self
+
+  def parent_cannot_be_self
+    if parent == self
+      errors.add(:parent, "cannot be self")
+    end
+  end
 end
