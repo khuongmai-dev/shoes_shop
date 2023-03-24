3eeb70160f66ddafa01fe5e8969e9ec69e768eef
Khuong Mai <maingockhuong89@gmail.com>
2023-03-24 16:51:48 -0500

    validate shopping_cart_item

--- app/models/shopping_cart_item.rb
+++ app/models/shopping_cart_item.rb
@@ -1,3 +1,8 @@
 class ShoppingCartItem < ApplicationRecord
+  validates :shopping_cart_id, :product_item_id, presence: true
+
   belongs_to :product_item
+
+  has_many :shopping_carts, dependent: :destroy
+  has_many :shop_users, through: :shopping_carts
 end
