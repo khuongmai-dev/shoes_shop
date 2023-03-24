a4c3dd1c4eb442f22ca0efb4388ff433f8de303e
Khuong Mai <maingockhuong89@gmail.com>
2023-03-24 17:03:38 -0500

    link active_admin to Brand

---
+++ app/admin/brands.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Brand do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :parent_id, :name, :brand_image
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:parent_id, :name, :brand_image]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
