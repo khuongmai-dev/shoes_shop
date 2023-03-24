81aaacb6d6a5b0bc45cd09d3ae3ab0c8be69be7e
Khuong Mai <maingockhuong89@gmail.com>
2023-03-24 11:09:51 -0500

    validate shop_user, address, user_address

--- app/models/shop_user.rb
+++ app/models/shop_user.rb
@@ -1,2 +1,11 @@
 class ShopUser < ApplicationRecord
+  validates :email_address, :phone_number, :password, presence: true
+
+  has_many :user_addresses, dependent: :delete_all
+  has_many :addresses, through: :user_addresses
+  has_many :shopping_carts, dependent: :delete_all
+  has_many :shopping_cart_items, through: :shopping_carts
+  has_many :user_reviews, dependent: :delete_all
+  has_many :order_lines, through: :user_reviews
+  has_many :shop_orders, :user_payment_methods, dependent: :delete_all
 end
--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -1,4 +1,6 @@
 class UserAddress < ApplicationRecord
   belongs_to :shop_user
   belongs_to :address
+
+  validates :shop_user_id, :address_id, presence: true
 end
