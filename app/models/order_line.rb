56de1c8fdf3f5a0e55fa0b41bbc1e8ba0618553a
Khuong Mai <maingockhuong89@gmail.com>
2023-03-24 15:21:04 -0500

    validate OrderLine model

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,4 +1,8 @@
 class OrderLine < ApplicationRecord
+  validates :quantity, :price, :shop_order_id, :product_item_id, presence: true
+
   belongs_to :product_item
   belongs_to :shop_order
+
+  has_many :user_reviews, dependent: :destroy
 end
